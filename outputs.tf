1a58fa3540d01b48efd1ebcaaaecbd3e3c9c9851
Lee Fowler <lee.fowler@hashicorp.com>
2026-01-14 12:07:17 -0500

    temporarily disable kubernetes provider outputs

--- outputs.tf
+++ outputs.tf
@@ -32,8 +32,10 @@ output "cluster_admin_password" {
   sensitive   = true
 }
 
-output "cluster_token" {
-  description = "Service account token for Terraform automation. Only available when create_kubernetes_resources = true."
-  value       = var.create_kubernetes_resources ? kubernetes_secret_v1.terraform_token[0].data["token"] : null
-  sensitive   = true
-}
+### Kubernetes Outputsß
+
+# output "cluster_token" {
+#   description = "Service account token for Terraform automation. Only available when create_kubernetes_resources = true."
+#   value       = var.create_kubernetes_resources ? kubernetes_secret_v1.terraform_token[0].data["token"] : null
+#   sensitive   = true
+# }
